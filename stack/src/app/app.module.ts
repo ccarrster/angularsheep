3ecfbcf2cd3e93b986c575723006b5ab9702a21d
ccarr <ccarrster@gmail.com>
2020-12-09 11:33:33 -0500

    Game details

--- stack/src/app/app.module.ts
+++ stack/src/app/app.module.ts
@@ -4,16 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    GameDetailsComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-    	{ path: '', component: GameComponent}
+    	{ path: '', component: GameComponent},
+    	{ path: 'game', component: GameDetailsComponent}
     ])
   ],
   providers: [],
